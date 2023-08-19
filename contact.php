e640f5e7f22944d63b88b25527c048abb29bb670
angle <64560416+angelina-kong@users.noreply.github.com>
2023-08-19 17:17:41 -0400

    modified:   contact.php 	modified:   index.html

--- contact.php
+++ contact.php
@@ -1,8 +1,10 @@
 <?php 
 $name = $email = $subject = $message = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
+if (isset($_POST['Email'])) {
+
     $email_to = "angelinakong905@gmail.com";
+    $email_subject = "Website";
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -27,15 +29,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $emailErr = "Invalid email format";
         }
     }  
-    
-    if (empty($_POST["subject"])) {
-        $subjectErr = "Please enter a subject";
-    } else {
-        $subject = test_input($_POST["subject"]);
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$subject)) {
-            $subjectErr = "Invalid name format, please include only letters or spaces";
-        }
-    }
     if (empty($_POST["message"])) {
         $messageErr = "Please enter a message";
     } else {
@@ -51,14 +44,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     
     $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Subject: " . clean_string($subject) . "\n";
     $email_message .= "Email: " . clean_string($email) . "\n";
     $email_message .= "Message: " . clean_string($message) . "\n";
     
     $headers = 'From: ' . $email . "\r\n" .
         'Reply-To: ' . $email . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $subject, $email_message, $headers);
+    @mail($email_to, $email_subject, $email_message, $headers);
 ?>
   Thank you for contacting! A respond will be made as soon as possible.
 
